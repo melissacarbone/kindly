2cda629b0bb9379b2a6d6b236fbcea13d4e4a810
Melissa Carbone <mcarbone1@gmail.com>
2014-01-02 09:02:58 -0500

    completed acceptance tests for user registration

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -24,10 +24,24 @@ feature 'User signs up' do
     expect(page).to have_content('Sign Out')
   end
 
-  scenario 'required information is not supplied'
+  scenario 'required information is not supplied' do
+    visit root_path
+    click_link 'Sign Up'
+    click_button 'Sign Up'
 
-  scenario 'password confirmation does not match password'
+    expect(page).to have_content("can't be blank")
+    expect(page).to_not have_content("Sign Out")
+  end
 
+  scenario 'password confirmation does not match password' do
+    visit root_path
+    click_link 'Sign Up'
 
+    fill_in 'user_password', with: 'password'
+    fill_in 'Password Confirmation', with: 'somethingDifferent'
+    click_button 'Sign Up'
 
+    expect(page).to have_content("doesn't match")
+    expect(page).to_not have_content("Sign Out")
+  end
 end
