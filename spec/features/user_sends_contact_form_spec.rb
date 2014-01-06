bc5105641ca16bdd4426dc3a0e5460863e7e571c
Melissa Carbone <mcarbone1@gmail.com>
2014-01-06 15:28:50 -0500

    User clicks contact link from root path

--- spec/features/user_sends_contact_form_spec.rb
+++ spec/features/user_sends_contact_form_spec.rb
@@ -2,7 +2,8 @@ require 'spec_helper'
 
 feature 'User sends a contact form' do
   scenario 'with all required and valid fields' do
-    visit 'contacts/new'
+    visit root_path
+    click_link 'Contact Us!'
     fill_in 'Email', with: 'user@example.com'
     fill_in 'Subject', with: 'Hello'
     fill_in 'Description', with: 'I like this site.'
@@ -11,7 +12,7 @@ feature 'User sends a contact form' do
     click_button 'Send Form'
 
     expect(page).to have_content('Form was successfully submitted.')
-
+    expect(page).to have_content("We'd love to hear from you...")
   end
 
 end
