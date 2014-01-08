eb0d632126aedfd0b01bf5abce24a8576e8cf3de
Melissa Carbone <mcarbone1@gmail.com>
2014-01-08 15:27:38 -0500

    revise application controller organization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,12 @@ class ApplicationController < ActionController::Base
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    posts_path
+  end
+
   def after_sign_out_path_for(resource)
     new_user_session_path
-  end
-   def after_sign_in_path_for(resource)
-    posts_path
   end
 
   protected
