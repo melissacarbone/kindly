50bf9f8a08e0a648853abb13a432e4d22d1ffd88
Melissa Carbone <mcarbone1@gmail.com>
2014-01-18 16:28:48 -0500

    Move smtp config to production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,14 @@
 Kindly::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
   # Code is not reloaded between requests.
   config.cache_classes = true
 
